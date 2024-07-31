bed82df63518233091848e3c7f2e8bd68c4172e2
Skywalker <shura.pro.05@mail.ru>
2024-07-31 19:56:34 +0500

    First commit

---
+++ base/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home,name='home'),
+    path('posts/', views.posts, name='posts'),
+    path('post/<slug:slug>/', views.post, name='post'),
+    path('profile/', views.profile, name='profile'),
+
+    #CRUD PATH
+
+    path('create_post/', views.createPost, name='create_post'),
+    path('update_post/<slug:slug>', views.updatePost, name='update_post'),
+    path('delete_post/<slug:slug>', views.deletePost, name='delete_post'),
+
+    path('send_email/', views.sendEmail, name='send_email')
+]
